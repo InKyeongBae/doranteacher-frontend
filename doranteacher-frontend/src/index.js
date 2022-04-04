83eecd5f7b641f960bb9055d0795ec019c1a420e
Koeunseooooo <rhdmstj17@cau.ac.kr>
2022-04-04 21:32:15 +0900

    feat : provider 컴포넌트를 통한 리덕스 스토어 생성 & _actions와 _reducers 폴더 생성

--- doranteacher-frontend/src/index.js
+++ doranteacher-frontend/src/index.js
@@ -2,4 +2,34 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+import { Provider } from 'react-redux';
+import { applyMiddleware, createStore } from 'redux';
+
+// 리덕스에서 필요한 미들웨어
+import promiseMiddleware from 'redux-promise';
+import ReduxThunk from 'redux-thunk';
+
+// reducer 불러오기
+import Reducer from './_reducers';
+
+// 미들웨어를 적용한 redux store를 만드는 과정
+const createStoreWithMiddleware = applyMiddleware(promiseMiddleware, ReduxThunk)(createStore);
+
 ReactDOM.render(<App />, document.getElementById('root'));
+
+ReactDOM.render(
+    <React.StrictMode>
+      {/* Provider를 이용해 리덕스 적용 */}
+      <Provider
+        store={createStoreWithMiddleware(
+          Reducer,
+          // 크롬 브라우저에서 디버깅 도구를 이용할 수 있도록 하는 도
+          window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+        )}
+      >
+        <App />
+      </Provider>
+    </React.StrictMode>,
+    document.getElementById('root'),
+  );
+  
