90e13e8d9d69c07ce209d30b4c4d80d28c082483
InKyeongBae <inkkyung@gmail.com>
2022-04-07 00:56:36 +0900

    Feat : 그림판 한 줄 높이 300px로 고정

--- doranteacher-frontend/src/components/Paint.js
+++ doranteacher-frontend/src/components/Paint.js
@@ -1,74 +1,58 @@
-import React, { useState } from "react";
-import "./Paint.css";
-import "./literallycanvas.css";
+import React, { useState } from 'react';
+import { withTheme } from 'styled-components';
+import './literallycanvas.css';
 
 // Documentation: http://literallycanvas.com/
-const LC = require("literallycanvas");
+const LC = require('literallycanvas');
 let _lc = null;
 
 function Paint() {
 	const [images, setImages] = useState([]);
 
-	const onInit = lc => {
-	  _lc = lc;
+	const onInit = (lc) => {
+		_lc = lc;
+		console.log(lc);
 	};
-  
-	const onSave = event => {
-	  if (!_lc) return;
-	  const img = _lc.getImage();
-	  if (!img) return;
-	  try {
-		const imgData = img.toDataURL();
-		setImages([...images, imgData]);
-	  } catch (err) {
-		console.log(err);
-	  }
+
+	const onSave = (event) => {
+		if (!_lc) return;
+		const img = _lc.getImage();
+		if (!img) return;
+		try {
+			const imgData = img.toDataURL();
+			setImages([...images, imgData]);
+		} catch (err) {
+			console.log(err);
+		}
 	};
-  
-	const loadBackground = event => {
-	  if (!_lc) return;
-	  const img = new Image();
-	  img.src = "/img/map2.png";
-	  let shape = LC.createShape("Image", { x: 0, y: 0, image: img, scale: 0.5 });
-	  _lc.saveShape(shape);
-	};
-  
+
 	return (
-	  <div style={{ padding: 10 }}>
 		<div>
-		  <LC.LiterallyCanvasReactComponent
-			onInit={onInit}
-			imageURLPrefix="/img"
-		  />
+			<div>
+				<LC.LiterallyCanvasReactComponent onInit={onInit} backgroundColor="#ffffff" 
+				// 
+				imageSize={{width: null, height: 250}}
+				tools = {[
+					LC.tools.Pencil,
+					LC.tools.Eraser]}
+				imageURLPrefix="/img" />
+			</div>
+			<div style={{ marginTop: 10 }}>
+				<button style={{ width: 150, margin: 10, fontSize: 14 }} onClick={onSave}>
+					Save As Image
+				</button>
+				<button style={{ width: 150, margin: 10, fontSize: 14 }} onClick={() => setImages([])}>
+					Clear Images
+				</button>
+			</div>
+			<ul style={{ marginTop: 10, listStyleType: 'none' }}>
+				{images.map((img, index) => (
+					<li key={index}>
+						<img src={img} />
+					</li>
+				))}
+			</ul>
 		</div>
-		<div style={{ marginTop: 10 }}>
-		  <button
-			style={{ width: 150, margin: 10, fontSize: 14 }}
-			onClick={loadBackground}
-		  >
-			Load Map
-		  </button>
-		  <button
-			style={{ width: 150, margin: 10, fontSize: 14 }}
-			onClick={onSave}
-		  >
-			Save As Image
-		  </button>
-		  <button
-			style={{ width: 150, margin: 10, fontSize: 14 }}
-			onClick={() => setImages([])}
-		  >
-			Clear Images
-		  </button>
-		</div>
-		<ul style={{ marginTop: 10, listStyleType: "none" }}>
-		  {images.map((img, index) => (
-			<li key={index}>
-			  <img src={img} />
-			</li>
-		  ))}
-		</ul>
-	  </div>
 	);
 }
 
