ae781b8b8b2d2528e3149e45d5b123f5f7b17e04
InKyeongBae <inkkyung@gmail.com>
2022-04-08 21:48:48 +0900

    Feat : Progressbar 퍼센트값으로 빨간색 부분 올라가도록

--- doranteacher-frontend/src/components/Progressbar.js
+++ doranteacher-frontend/src/components/Progressbar.js
@@ -7,7 +7,7 @@ const HeaderProgress = styled.div`
 		font-size: 25px;
 		height: 42.5px;
 		color: black;
-		background: #F9DE4B;
+		background: #f9de4b;
 		outline: 0;
 		border: 0;
 		letter-spacing: 1px;
@@ -40,26 +40,27 @@ const HeaderProgress = styled.div`
 			border: 2px solid black;
 		}
 
-		/* &:after {
-			z-index:3;
+		&:after {
+			z-index: 3;
 			position: absolute;
 			text-align: right;
 			content: '';
-			width: 400px;
+			width: 450px;
 			height: 11px;
-			background: white;
-			transform: translate3d(0.2em, 0.15em, 1em);
+			/* 빨간색 부분 %가 더 많을 때는 (90deg, #E75244 70%, white 30%) 이런 식으로 작성 */
+			background: linear-gradient( to left, white 70%, #E75244 30%);
+			transform: translate3d(15px, 100%, 30em);
 			border-radius: 25px;
-		} */
+		}
 	}
 `;
 
 function Progressbar({ buttonText, ...rest }) {
-  return (
-	<HeaderProgress {...rest}>
-		<button className="button">{buttonText}</button>
-	</HeaderProgress>
-  );
+	return (
+		<HeaderProgress {...rest}>
+			<button className="button">{buttonText}</button>
+		</HeaderProgress>
+	);
 }
 
 export default Progressbar;
