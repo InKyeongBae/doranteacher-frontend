799475c78b3e9c77e17f33aaa2da08f9308b371f
Koeunseooooo <rhdmstj17@cau.ac.kr>
2022-04-12 15:19:08 +0900

    Style : tap size 4로 고정

--- doranteacher-frontend/src/App.js
+++ doranteacher-frontend/src/App.js
@@ -8,29 +8,29 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Brainstorm from "./pages/Brainstorm";
 
 const palette = {
-  red: "#E75244",
-  blue: "#367BBE",
-  purple: "#8491E0",
-  yellow: "#F9DE4B",
-  green: "#5DCB83",
-  white: "#ffffff",
-  black: "#000000",
+    red: "#E75244",
+    blue: "#367BBE",
+    purple: "#8491E0",
+    yellow: "#F9DE4B",
+    green: "#5DCB83",
+    white: "#ffffff",
+    black: "#000000",
 };
 
 function App() {
-  return (
-    <BrowserRouter>
-      <ThemeProvider theme={{ palette }}>
-        <Routes>
-          <Route path="/" element={<Mainpage />} />
-          <Route path="/login" element={<LoginPage />} />
-          <Route path="/signup" element={<Signuppage />} />
-          <Route path="/paint" element={<Paint />} />
-          <Route path="/first-step" element={<Brainstorm />} />
-        </Routes>
-      </ThemeProvider>
-    </BrowserRouter>
-  );
+    return (
+        <BrowserRouter>
+            <ThemeProvider theme={{ palette }}>
+                <Routes>
+                    <Route path="/" element={<Mainpage />} />
+                    <Route path="/login" element={<LoginPage />} />
+                    <Route path="/signup" element={<Signuppage />} />
+                    <Route path="/paint" element={<Paint />} />
+                    <Route path="/first-step" element={<Brainstorm />} />
+                </Routes>
+            </ThemeProvider>
+        </BrowserRouter>
+    );
 }
 
 export default App;
--- doranteacher-frontend/src/pages/Signuppage.js
+++ doranteacher-frontend/src/pages/Signuppage.js
@@ -3,78 +3,86 @@ import { useDispatch } from "react-redux";
 import { signupUser } from "../_actions/user_action";
 
 function Signuppage(props) {
-  const [Username, setUsername] = useState("");
-  const [Password, setPassword] = useState("");
-  const [ConfirmPassword, setConfirmPassword] = useState("");
+    const [Username, setUsername] = useState("");
+    const [Password, setPassword] = useState("");
+    const [ConfirmPassword, setConfirmPassword] = useState("");
 
-  const dispatch = useDispatch();
+    const dispatch = useDispatch();
 
-  // handler 함수들
-  const onUsernameHandler = (event) => {
-    setUsername(event.currentTarget.value);
-  };
-
-  const onPasswordHandler = (event) => {
-    setPassword(event.currentTarget.value);
-  };
-
-  const onConfirmPasswordHandler = (event) => {
-    setConfirmPassword(event.currentTarget.value);
-  };
-
-  const onSubmitHandler = (event) => {
-    // 태그의 기본 기능으로 리프레쉬 되는 것을 방지.
-    event.preventDefault();
-
-    if (Password !== ConfirmPassword) {
-      return alert("비밀번호와 비밀번호 확인은 같아야 해요!");
-    }
-
-    let body = {
-      username: Username,
-      password: Password,
+    // handler 함수들
+    const onUsernameHandler = (event) => {
+        setUsername(event.currentTarget.value);
     };
 
-    // action의 반환값을 dispatch해준다.
-    dispatch(signupUser(body)).then((response) => {
-      if (response.payload.loginSuccess) {
-        props.history.push("/login");
-      } else {
-        alert("회원가입에 실패했어요.");
-      }
-    });
-  };
+    const onPasswordHandler = (event) => {
+        setPassword(event.currentTarget.value);
+    };
 
-  return (
-    <div
-      style={{
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-        width: "100%",
-        height: "100vh",
-      }}
-    >
-      <form
-        style={{ display: "flex", flexDirection: "column" }}
-        onSubmit={onSubmitHandler}
-      >
-        <label>Username</label>
-        <input type="username" value={Username} onChange={onUsernameHandler} />
-        <label>Password</label>
-        <input type="password" value={Password} onChange={onPasswordHandler} />
-        <label>Confirm Password</label>
-        <input
-          type="password"
-          value={ConfirmPassword}
-          onChange={onConfirmPasswordHandler}
-        />
+    const onConfirmPasswordHandler = (event) => {
+        setConfirmPassword(event.currentTarget.value);
+    };
 
-        <br />
-        <button type="submit">Login</button>
-      </form>
-    </div>
-  );
+    const onSubmitHandler = (event) => {
+        // 태그의 기본 기능으로 리프레쉬 되는 것을 방지.
+        event.preventDefault();
+
+        if (Password !== ConfirmPassword) {
+            return alert("비밀번호와 비밀번호 확인은 같아야 해요!");
+        }
+
+        let body = {
+            username: Username,
+            password: Password,
+        };
+
+        // action의 반환값을 dispatch해준다.
+        dispatch(signupUser(body)).then((response) => {
+            if (response.payload.loginSuccess) {
+                props.history.push("/login");
+            } else {
+                alert("회원가입에 실패했어요.");
+            }
+        });
+    };
+
+    return (
+        <div
+            style={{
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+                width: "100%",
+                height: "100vh",
+            }}
+        >
+            <form
+                style={{ display: "flex", flexDirection: "column" }}
+                onSubmit={onSubmitHandler}
+            >
+                <label>Username</label>
+                <input
+                    type="username"
+                    value={Username}
+                    onChange={onUsernameHandler}
+                />
+                <label>Password</label>
+                <input
+                    type="password"
+                    value={Password}
+                    onChange={onPasswordHandler}
+                />
+                <label>Confirm Password</label>
+                <input
+                    type="password"
+                    value={ConfirmPassword}
+                    onChange={onConfirmPasswordHandler}
+                />
+
+                <br />
+                <button type="submit">Login</button>
+            </form>
+        </div>
+    );
 }
 
 export default Signuppage;
