9446ea7af2026e29f75967c57af837f3fa837ddb
Koeunseooooo <rhdmstj17@cau.ac.kr>
2022-04-12 19:14:54 +0900

    Design : 로그인 페이지 css 적용

--- doranteacher-frontend/src/pages/Loginpage.js
+++ doranteacher-frontend/src/pages/Loginpage.js
@@ -1,6 +1,55 @@
 import React, { useState } from "react";
 import { useDispatch } from "react-redux";
 import { loginUser } from "../_actions/user_action";
+import styled, { css, createGlobalStyle } from "styled-components";
+import Header from "../components/Header";
+import ShakingHands from "../components/ShakingHands";
+import GlobalStyle from "../components/GlobalStyle";
+
+const MainBlock = styled.div`
+    background: #f9de4b;
+`;
+
+const CenterLogo = styled.div`
+    text-align: center;
+
+    img {
+        width: 300px;
+        padding: 40px 0px;
+    }
+
+    .centercontent {
+        font-family: "NeoDunggeunmo";
+        font-style: normal;
+        font-weight: 400;
+        font-size: 30px;
+        line-height: 30px;
+    }
+`;
+
+const LoginUI = styled.div`
+    .loginform {
+        display: "flex";
+        justifycontent: "center";
+        alignitems: "center";
+        // width: "100%";
+        // height: "100vh";
+    }
+
+    .content {
+        font-family: "상상토끼 꽃집막내딸 OTF";
+        font-style: normal;
+        font-weight: 400;
+        font-size: 30px;
+        line-height: 30px;
+        background: #f9de4b;
+        // vertical-align: middle;
+    }
+
+    .input {
+        background: #f9de4b;
+    }
+`;
 
 const Loginpage = (props) => {
     // redux의 dispatch
@@ -39,35 +88,50 @@ const Loginpage = (props) => {
     };
 
     return (
-        <div
-            style={{
-                display: "flex",
-                justifyContent: "center",
-                alignItems: "center",
-                width: "100%",
-                height: "100vh",
-            }}
-        >
-            <form
-                style={{ display: "flex", flexDirection: "column" }}
-                onSubmit={onSubmitHandler}
-            >
-                <label>아이디</label>
-                <input
-                    type="username"
-                    value={Username}
-                    onChange={onUsernameHandler}
-                />
-                <label>비밀번호</label>
-                <input
-                    type="password"
-                    value={Password}
-                    onChange={onPasswordHandler}
-                />
-                <br />
-                <button type="submit">로그인</button>
-            </form>
-        </div>
+        <>
+            <GlobalStyle backColor="yellow" />
+            <Header isIcon />
+            <MainBlock>
+                <CenterLogo>
+                    <div className="centerlogo">
+                        <img className="doranLogo" src="/img/doranlogo.png" />
+                    </div>
+                    <div className="centercontent">
+                        AI 도란쌤과 함께
+                        <br />
+                        일기 마스터하기
+                    </div>
+                </CenterLogo>
+
+                <LoginUI onSubmit={onSubmitHandler}>
+                    <div className="loginform">
+                        <form
+                            style={{ display: "flex", flexDirection: "column" }}
+                            onSubmit={onSubmitHandler}
+                        >
+                            <label className="content">아이디</label>
+                            <input
+                                className="input"
+                                type="username"
+                                value={Username}
+                                onChange={onUsernameHandler}
+                            />
+                            <label className="content">비밀번호</label>
+                            <input
+                                className="input"
+                                type="password"
+                                value={Password}
+                                onChange={onPasswordHandler}
+                            />
+                            <br />
+                            <button type="submit" className="content">
+                                로그인
+                            </button>
+                        </form>
+                    </div>
+                </LoginUI>
+            </MainBlock>
+        </>
     );
 };
 
