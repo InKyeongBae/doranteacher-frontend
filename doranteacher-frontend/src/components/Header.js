ee5931ecd0ecb78a88cd37a6386e27b5414f58ed
InKyeongBae <inkkyung@gmail.com>
2022-04-14 13:08:40 +0900

    Fix : main 브랜치 머지 후 Header 충돌 에러 해결

--- doranteacher-frontend/src/components/Header.js
+++ doranteacher-frontend/src/components/Header.js
@@ -3,15 +3,16 @@ import styled, { ThemeConsumer, ThemeProvider } from 'styled-components';
 import Button from './Button';
 import ImgButton from './ImgButton';
 import Progressbar from './Progressbar';
+import { Link } from 'react-router-dom';
 
 const HeaderBlock = styled.div`
-    position: sticky;
-    top: 0;
-    color: black;
-    padding: 20px 50px 15px 50px;
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
+	position: sticky;
+	top: 0;
+	color: black;
+	padding: 20px 50px 15px 50px;
+	display: flex;
+	justify-content: space-between;
+	align-items: center;
 `;
 
 const HeaderIcon = styled.div`
@@ -33,33 +34,48 @@ const HeaderIcon = styled.div`
 `;
 
 const HeaderButtons = styled.div`
-    div {
-        display: inline-block;
-        padding-left: 20px;
-        padding-bottom: 5px;
-    }
+	div {
+		display: inline-block;
+		padding-left: 20px;
+		padding-bottom: 5px;
+	}
 `;
 
-function Header({ isIcon, isProgress, isSignup, isLogin, isLogout, isImgBtn }) {
+const StyledLink = styled(Link)`
+	text-decoration: none;
+	color: black;
+	&:focus,
+	&:hover,
+	&:visited,
+	&:link,
+	&:active {
+		text-decoration: none;
+		color: black;
+	}
+`;
+
+function Header({ isIcon, isProgress, isSignup, isLogin, isLogout, isSetting }) {
 	return (
 		<HeaderBlock>
-			<HeaderIcon className="mainIcon" >
-				{isIcon ? (
-					<>
-						<img className="doranIcon" src="/img/header-doran-face.png" />
-						<span className="doranIconName">도란쌤</span>
-					</>
-				) : (
-					<img className="doranIcon" src="/img/doranlogo.png" />
-				)}
+			<HeaderIcon className="mainIcon">
+				<StyledLink to="/">
+					{isIcon ? (
+						<>
+							<img className="doranIcon" src="/img/header-doran-face.png" />
+							<span className="doranIconName">도란쌤</span>
+						</>
+					) : (
+						<img className="doranIcon" src="/img/doranlogo.png" />
+					)}
+				</StyledLink>
 			</HeaderIcon>
 
 			<HeaderButtons className="mainHeader">
 				{isProgress ? <Progressbar buttonText="2. 글감 찾기"></Progressbar> : null}
-				{isSignup ? <Button buttonText="회원가입" outputColor="red"></Button> : null}
-				{isLogin ? <Button buttonText="로그인" outputColor="purple"></Button> : null}
+				<Link to="/signup">{isSignup ? <Button buttonText="회원가입" outputColor="red"></Button> : null}</Link>
+				<Link to="/login">{isLogin ? <Button buttonText="로그인" outputColor="purple"></Button> : null}</Link>
 				{isLogout ? <Button buttonText="로그아웃" outputColor="purple"></Button> : null}
-				{isImgBtn ? <ImgButton setting={true} undo={false} outputColor="white"></ImgButton> : null}
+				<Link to="/setting">{isSetting ? <ImgButton setting={true} undo={false} outputColor="white"></ImgButton> : null}</Link>
 			</HeaderButtons>
 		</HeaderBlock>
 	);
