88eef7165658dda2cb2a316bf97642f3318467c2
InKyeongBae <inkkyung@gmail.com>
2022-04-21 23:03:11 +0900

    Fix : proxy 8080 포트 관련 오류 수정

--- doranteacher-frontend/src/App.js
+++ doranteacher-frontend/src/App.js
@@ -1,54 +1,54 @@
-import React from "react";
-import { createGlobalStyle, ThemeProvider, css } from "styled-components";
-import Mainpage from "./pages/Mainpage";
-import LoginPage from "./pages/Loginpage";
-import Signuppage from "./pages/Signuppage";
-import NotFound from "./pages/NotFound";
-import Setting from "./pages/Setting";
-import WritingStart from "./pages/WritingStart";
-import Paint from "./components/Paint";
-import ProgressBar from "./components/ProgressBar";
-import { BrowserRouter, Routes, Route, Outlet } from "react-router-dom";
-import Brainstorm from "./pages/Brainstorm";
+import React from 'react';
+import { createGlobalStyle, ThemeProvider, css } from 'styled-components';
+import Mainpage from './pages/Mainpage';
+import LoginPage from './pages/Loginpage';
+import Signuppage from './pages/Signuppage';
+import NotFound from './pages/NotFound';
+import Setting from './pages/Setting';
+import WritingStart from './pages/WritingStart';
+import Paint from './components/Paint';
+import ProgressBar from './components/ProgressBar';
+import { BrowserRouter, Routes, Route, Outlet } from 'react-router-dom';
+import Brainstorm from './pages/Brainstorm';
 
 const palette = {
-    red: "#E75244",
-    blue: "#367BBE",
-    purple: "#8491E0",
-    yellow: "#F9DE4B",
-    green: "#5DCB83",
-    white: "#ffffff",
-    black: "#000000",
+	red: '#E75244',
+	blue: '#367BBE',
+	purple: '#8491E0',
+	yellow: '#F9DE4B',
+	green: '#5DCB83',
+	white: '#ffffff',
+	black: '#000000',
 };
 
 function Writing() {
-    return (
-        <>
-            <Outlet />
-        </>
-    );
+	return (
+		<>
+			<Outlet />
+		</>
+	);
 }
 
 function App() {
-    return (
-        <BrowserRouter>
-            <ThemeProvider theme={{ palette }}>
-                <Routes>
-                    <Route path="/" element={<Mainpage />} />
-                    <Route path="/login" element={<LoginPage />} />
-                    <Route path="/signup" element={<Signuppage />} />
-                    <Route path="/paint" element={<Paint />} />
-                    <Route path="/setting" element={<Setting />} />
-                    <Route path="/writing" element={<Writing />}>
-                        <Route path="start" element={<WritingStart />} />
-                        <Route path="first-step" element={<Brainstorm />} />
-                        {/* <Route path="*" element={<NotFound />} /> */}
-                    </Route>
-                    <Route path="*" element={<NotFound />} />
-                </Routes>
-            </ThemeProvider>
-        </BrowserRouter>
-    );
+	return (
+		<BrowserRouter>
+			<ThemeProvider theme={{ palette }}>
+				<Routes>
+					<Route path="/" element={<Mainpage />} />
+					<Route path="/login" element={<LoginPage />} />
+					<Route path="/signup" element={<Signuppage />} />
+					<Route path="/paint" element={<Paint />} />
+					<Route path="/setting" element={<Setting />} />
+					<Route path="/writing" element={<Writing />}>
+						<Route path="start" element={<WritingStart />} />
+						<Route path="first-step" element={<Brainstorm />} />
+						{/* <Route path="*" element={<NotFound />} /> */}
+					</Route>
+					<Route path="*" element={<NotFound />} />
+				</Routes>
+			</ThemeProvider>
+		</BrowserRouter>
+	);
 }
 
 export default App;
