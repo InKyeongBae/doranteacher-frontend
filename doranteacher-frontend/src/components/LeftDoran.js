99badbad82ca23f957e3283cb370717c47ae55b1
InKyeongBae <inkkyung@gmail.com>
2022-04-29 13:19:46 +0900

    Feat : running 상태 close로 변경

--- doranteacher-frontend/src/components/LeftDoran.js
+++ doranteacher-frontend/src/components/LeftDoran.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
-import axios from "axios";
+import axios from 'axios';
 
 const LeftDoranStyle = styled.div`
 	position: fixed;
@@ -14,34 +14,36 @@ const LeftDoranStyle = styled.div`
 	}
 `;
 
+const context = new AudioContext();
+
 async function speaking(text) {
 	const nowText = text[0].question;
 	const xmlData = '<speak>' + nowText + ' 단어로 한 번 적어볼까요?</speak>';
 	try {
+		// if (context.state === 'running') context.close();
 		const { data } = await axios.post('https://kakaoi-newtone-openapi.kakao.com/v1/synthesize', xmlData, {
 			headers: {
 				'Content-Type': 'application/xml',
-				Authorization: `KakaoAK cca71e7ca7380f77c6c02e690962ab9a`,
+				Authorization: `KakaoAK 385ba3ec71547eb9ff85151e5d2834fc`,
 			},
 			responseType: 'arraybuffer',
 		});
-		const context = new AudioContext();
+		console.log(context.state);
 		context.decodeAudioData(data, (buffer) => {
 			const source = context.createBufferSource();
 			source.buffer = buffer;
 			source.connect(context.destination);
 			source.start(0);
-			return source;
 		});
 	} catch (e) {
 		console.error(e.message);
 	}
-};
+}
 
-function LeftDoran({text}) {
+function LeftDoran({ text }) {
 	return (
 		<LeftDoranStyle>
-			<div className="leftDoran" onClick={() => speaking(text)}/>
+			<div className="leftDoran" onClick={() => speaking(text)} />
 		</LeftDoranStyle>
 	);
 }
