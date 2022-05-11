e112a29b251e20446214b3695e2faefff247e45a
Koeunseooooo <rhdmstj17@cau.ac.kr>
2022-05-11 21:09:38 +0900

    Feat: button 컴포넌트 너비,높이 관련 props 추가

--- doranteacher-frontend/src/pages/WritingStart.js
+++ doranteacher-frontend/src/pages/WritingStart.js
@@ -1,68 +1,70 @@
-import React, { useState } from 'react';
-import { useDispatch } from 'react-redux';
-import Button from '../components/Button';
-import { signupUser } from '../_actions/user_action';
-import styled, { css, createGlobalStyle } from 'styled-components';
-import Header from '../components/Header';
-import GlobalStyle from '../components/GlobalStyle';
-import ProgressBar from '../components/ProgressBar';
-import { Link, useNavigate } from 'react-router-dom';
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
+import Button from "../components/Button";
+import { signupUser } from "../_actions/user_action";
+import styled, { css, createGlobalStyle } from "styled-components";
+import Header from "../components/Header";
+import GlobalStyle from "../components/GlobalStyle";
+import ProgressBar from "../components/ProgressBar";
+import { Link, useNavigate } from "react-router-dom";
 
 const ColorStyles = css`
-	${({ theme, inputColor, outputColor }) => {
-		const incolor = theme.palette[inputColor];
-		const outcolor = theme.palette[outputColor];
-		return css`
-			color: black;
-			background: ${incolor};
-			&:hover {
-				background: ${outcolor};
-			}
-		`;
-	}}
+    ${({ theme, inputColor, outputColor }) => {
+        const incolor = theme.palette[inputColor];
+        const outcolor = theme.palette[outputColor];
+        return css`
+            color: black;
+            background: ${incolor};
+            &:hover {
+                background: ${outcolor};
+            }
+        `;
+    }}
 `;
 const LeftDoran = styled.div`
-	position: fixed;
-	bottom: 0;
-	padding: 0px 0px 0px 30px;
+    position: fixed;
+    bottom: 0;
+    padding: 0px 0px 0px 30px;
 
-	.leftDoran {
-		width: 200px;
-		height: 400px;
-		background: url('/img/doranSsam.png') no-repeat 0 0 / auto 400px;
-	}
+    .leftDoran {
+        width: 200px;
+        height: 400px;
+        background: url("/img/doranSsam.png") no-repeat 0 0 / auto 400px;
+    }
 `;
 
 function WritingStart() {
-	const navigate = useNavigate('');
-	return (
-		<>
-			<GlobalStyle backColor="red" />
-			<LeftDoran>
-				<div className="leftDoran" />
-			</LeftDoran>
-			<Header
-				isProgress
-				isLogout
-				isImgBtn
-				progress={
-					<ProgressBar
-						progressText={'1. 일기쓰기'}
-						progressWidth={'12.5'}
-						progressColor={'#E75244'}
-						backColor="red"
-					></ProgressBar>
-				}
-			/>
-			<Button
-				buttonText="다음"
-				type="submit"
-				outputColor="red"
-				className="button"
-				onClick={() => navigate('/writing/first-step')}
-			></Button>
-			{/* <MainBlock> */}
-			{/* <div className="today">오늘은,</div>
+    const navigate = useNavigate("");
+    return (
+        <>
+            <GlobalStyle backColor="red" />
+            <LeftDoran>
+                <div className="leftDoran" />
+            </LeftDoran>
+            <Header
+                isProgress
+                isLogout
+                isImgBtn
+                progress={
+                    <ProgressBar
+                        progressText={"1. 일기쓰기"}
+                        progressWidth={"12.5"}
+                        progressColor={"#E75244"}
+                        backColor="red"
+                    ></ProgressBar>
+                }
+            />
+            <Button
+                buttonText="다음"
+                type="submit"
+                outputColor="purple"
+                height="90px"
+                weight="90px"
+                className="button"
+                onClick={() => navigate("/writing/first-step")}
+            ></Button>
+            {/* <MainBlock> */}
+            {/* <div className="today">오늘은,</div>
                 <div className="dateContent">
                     <DateButton>
                         <div className="date">4</div>
@@ -112,9 +114,9 @@ function WritingStart() {
                         onClick={() => navigate("/writing/first-step")}
                     ></Button>
                 </div> */}
-			{/* </MainBlock> */}
-		</>
-	);
+            {/* </MainBlock> */}
+        </>
+    );
 }
 
 export default WritingStart;
