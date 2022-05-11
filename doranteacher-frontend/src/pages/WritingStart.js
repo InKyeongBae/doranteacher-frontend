857116300dc35a8796ff0b9d5b8af58712c7b808
Koeunseooooo <rhdmstj17@cau.ac.kr>
2022-05-11 22:41:17 +0900

    Remove : SimpleButton 컴포넌트 삭제

--- doranteacher-frontend/src/pages/WritingStart.js
+++ doranteacher-frontend/src/pages/WritingStart.js
@@ -76,11 +76,14 @@ function WritingStart() {
                 <section>
                     <h1 className="content"> 오늘은 </h1>
                     <div className="input-box">
-                        <input
-                            type="date"
-                            value={date}
-                            onClick={(e) => setDate(e.target.value)}
-                        ></input>
+                        <div>
+                            <input
+                                className="input-date"
+                                type="date"
+                                value={date}
+                                onClick={(e) => setDate(e.target.value)}
+                            ></input>
+                        </div>
                     </div>
                 </section>
             </Calendar>
@@ -101,3 +104,77 @@ function WritingStart() {
 }
 
 export default WritingStart;
+
+// import React from "react";
+// import styled, { css } from "styled-components";
+
+// const ColorStyles = css`
+//     ${({ theme, inputColor, height, width }) => {
+//         const incolor = theme.palette[inputColor];
+//         return css`
+//             color: black;
+//             background: ${incolor};
+//             height: ${height};
+//             width: ${width};
+//         `;
+//     }}
+// `;
+
+// const HeaderButtons = styled.div`
+//     .button {
+//         min-width: 160px;
+//         font-size: 25px;
+//         height: 42.5px;
+//         width: 90px;
+//         ${ColorStyles};
+//         outline: 0;
+//         border: 0;
+//         letter-spacing: 1px;
+//         // cursor: pointer;
+//         position: relative;
+//         padding: 3px 35px;
+//         font-family: "상상토끼 꽃집막내딸 OTF";
+//         font-style: normal;
+//         font-weight: 400;
+//         border-radius: 25px;
+//         border: 2px solid black;
+//         transition: transform 0.2s cubic-bezier(0, 0, 0.7, 1);
+
+//         &:before {
+//             z-index: -1;
+//             position: absolute;
+//             top: 0;
+//             right: 0;
+//             bottom: 0;
+//             left: 0;
+//             content: "";
+//             width: 98%;
+//             height: 98%;
+//             position: absolute;
+//             background: white;
+//             transform: translate3d(0.2em, 0.15em, 1em);
+//             border-radius: 25px;
+//             border: 2px solid black;
+//             transition: transform 0.2s cubic-bezier(0, 0, 0.7, 1);
+//             &:active {
+//                 z-index: -1;
+//             }
+//         }
+//     }
+// `;
+
+// function SimpleButton({ buttonText, ...rest }) {
+//     return (
+//         <HeaderButtons {...rest}>
+//             <div className="button">{buttonText}</div>
+//         </HeaderButtons>
+//     );
+// }
+
+// SimpleButton.defaultProps = {
+//     inputColor: "yellow",
+//     height: "42.5px",
+//     width: "90px",
+// };
+
+// export default SimpleButton;
