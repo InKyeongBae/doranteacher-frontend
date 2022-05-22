1d62c828c26b3e51985311834e70cb4c41184a55
InKyeongBae <inkkyung@gmail.com>
2022-05-22 15:10:05 +0900

    Feat : 다음 단계 넘어갈 때 조건에 만족하면 writing/save 로 이동

--- doranteacher-frontend/src/pages/step1/Step1List.js
+++ doranteacher-frontend/src/pages/step1/Step1List.js
@@ -1,5 +1,7 @@
 import React, { useMemo } from 'react';
+import { useNavigate } from 'react-router-dom';
 import styled from 'styled-components';
+import Button from '../../components/Button';
 import ImgButton from '../../components/ImgButton';
 import { useSentenceDispatch, useSentenceState } from '../diarycontents/SentenceContext';
 
@@ -23,6 +25,7 @@ function Step1List(props) {
 	const step1Qs = useSentenceState();
 	const dispatch = useSentenceDispatch();
 	const active = step1Qs.filter((sentence) => sentence.active);
+	const navigate = useNavigate('');
 	function onChange(originId, nextId) {
 		dispatch({
 			type: 'CHANGE_ACTIVE',
@@ -51,17 +54,30 @@ function Step1List(props) {
 						style={{ visibility: step1Qs.id === 1 ? 'hidden' : 'show' }}
 					/>
 					<div className="questioncontent">{step1Qs.question}</div>
-					<ImgButton
-						next
-						inputColor="green"
-						outputColor="purple"
-						onClick={
-							active[0].answer === ''
-								? () => props.nextNotify()
-								: () => onChange(step1Qs.id, step1Qs.id + 1)
-						}
-						style={{ visibility: step1Qs.id === 5 ? 'hidden' : 'show' }}
-					/>
+					{active[0].id === 5 ? (
+						<Button
+							inputColor="green"
+							outputColor="purple"
+							buttonText={'다음'}
+							onClick={
+								active[0].answer === ''
+									? () => props.levelNotify()
+									: () => navigate('/writing/save')
+							}
+						/>
+					) : (
+						<ImgButton
+							next
+							inputColor="green"
+							outputColor="purple"
+							onClick={
+								active[0].answer === ''
+									? () => props.nextNotify()
+									: () => onChange(step1Qs.id, step1Qs.id + 1)
+							}
+							style={{ visibility: step1Qs.id === 5 ? 'hidden' : 'show' }}
+						/>
+					)}
 				</div>
 				<div className="explain-text" style={{ textAlign: 'center' }}>
 					<div className="description" style={{ display: 'inline-block' }}>
