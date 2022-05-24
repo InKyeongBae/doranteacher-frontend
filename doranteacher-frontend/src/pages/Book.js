49e1bc92309a47eb3703513b8c1aaff7fb446a0e
Koeunseooooo <rhdmstj17@cau.ac.kr>
2022-05-25 04:39:33 +0900

    Feat: DiaryList와 Book 컴포넌트 생성

---
+++ doranteacher-frontend/src/pages/Book.js
@@ -0,0 +1,15 @@
+import React, { useState } from "react";
+import styled, { css, createGlobalStyle } from "styled-components";
+import Header from "../components/Header";
+import Button from "../components/Button";
+import GlobalStyle from "../components/GlobalStyle";
+import { useNavigate, Link } from "react-router-dom";
+import LeftDoran from "../components/LeftDoran";
+import ProgressBar from "../components/ProgressBar";
+import ImageUpload from "../components/ImageUpload";
+
+function Book() {
+    return <>book</>;
+}
+
+export default Book;
