6ab89bf4b051c6fb36fef8dec1bebbb99b2f8c4e
Koeunseooooo <rhdmstj17@cau.ac.kr>
2022-05-25 05:44:34 +0900

    Feat: Book뷰 DiaryList뷰 레이아웃 구성 완료

--- doranteacher-frontend/src/pages/Book.js
+++ doranteacher-frontend/src/pages/Book.js
@@ -8,15 +8,43 @@ import LeftDoran from "../components/LeftDoran";
 import ProgressBar from "../components/ProgressBar";
 import ImageUpload from "../components/ImageUpload";
 
-const MainBlock = styled.div``;
+const MainBlock = styled.div`
+    display: flex;
+    justify-content: space-between;
 
+    .rightSide {
+    }
+
+    .buttons {
+        margin-top: 50px;
+        margin-right: 225px;
+        display: flex;
+    }
+
+    .button {
+        margin-right: 20px;
+        margin-left: 20px;
+    }
+
+    .on {
+        background: #e75244;
+        transition: all 0.1s cubic-bezier(0, 0, 0.7, 1);
+        top: 4px;
+        left: 3.5px;
+        &:before {
+            top: -5px;
+            left: -4.7px;
+        }
+    }
+`;
 const BigDoran = styled.div`
     .bigDoran {
-        height: 690px;
+        height: 700px;
     }
 `;
 
 function Book() {
+    const navigate = useNavigate("");
     return (
         <>
             <GlobalStyle backColor="yellow" />
@@ -29,6 +57,29 @@ function Book() {
                             src="/img/big-doran-heart-right.png"
                         />
                     </BigDoran>
+                </div>
+                <div className="rightSide">
+                    <div className="buttons">
+                        <div className="button">
+                            <Button
+                                buttonText="이달의 일기"
+                                width="250px"
+                                height="50px"
+                                onClick={() => navigate("/diary-list")}
+                            ></Button>
+                        </div>
+                        <div className="button">
+                            <Button
+                                buttonText="책으로 엮어보기"
+                                width="250px"
+                                height="50px"
+                                extraClassName="on"
+                            ></Button>
+                        </div>
+                        <div className="books">
+                            {/* 책 리스트 출력하는 곳 */}
+                        </div>
+                    </div>
                 </div>
                 <div className="middleSide"></div>
                 <div className="rightSide"></div>
--- doranteacher-frontend/src/pages/DiaryList.js
+++ doranteacher-frontend/src/pages/DiaryList.js
@@ -8,8 +8,36 @@ import LeftDoran from "../components/LeftDoran";
 import ProgressBar from "../components/ProgressBar";
 import ImageUpload from "../components/ImageUpload";
 
-const MainBlock = styled.div``;
+const MainBlock = styled.div`
+    display: flex;
+    justify-content: space-between;
 
+    .rightSide {
+    }
+
+    .buttons {
+        margin-top: 50px;
+        margin-left: 380px;
+        // margin-right: 200px;
+        display: flex;
+    }
+
+    .button {
+        margin-right: 20px;
+        margin-left: 20px;
+    }
+
+    .on {
+        background: #e75244;
+        transition: all 0.1s cubic-bezier(0, 0, 0.7, 1);
+        top: 4px;
+        left: 3.5px;
+        &:before {
+            top: -5px;
+            left: -4.7px;
+        }
+    }
+`;
 const BigDoran = styled.div`
     .bigDoran {
         height: 700px;
@@ -17,13 +45,35 @@ const BigDoran = styled.div`
 `;
 
 function DiaryList() {
+    const navigate = useNavigate("");
     return (
         <>
             <GlobalStyle backColor="yellow" />
             <Header isUndo backColor="yellow" />
             <MainBlock>
-                <div className="leftSide"></div>
-                <div className="middleSide"></div>
+                <div className="leftSide">
+                    <div className="buttons">
+                        <div className="button">
+                            <Button
+                                buttonText="이달의 일기"
+                                width="250px"
+                                height="50px"
+                                extraClassName="on"
+                            ></Button>
+                        </div>
+                        <div className="button">
+                            <Button
+                                buttonText="책으로 엮어보기"
+                                width="250px"
+                                height="50px"
+                                onClick={() => navigate("/book-list")}
+                            ></Button>
+                        </div>
+                        <div className="diarys">
+                            {/* 일기 리스트 출력하는 곳 */}
+                        </div>
+                    </div>
+                </div>
                 <div className="rightSide">
                     <BigDoran>
                         <img
