514ac4f177386f1489c1479240d73c0c4ab8b602
Koeunseooooo <rhdmstj17@cau.ac.kr>
2022-06-01 15:40:15 +0900

    Feat: 프로그래스바 단계별 너비 재설정

--- doranteacher-frontend/src/pages/step1/Step1.js
+++ doranteacher-frontend/src/pages/step1/Step1.js
@@ -1,226 +1,230 @@
-import React, { useState } from 'react';
-import styled from 'styled-components';
-import Header from '../../components/Header';
-import GlobalStyle from '../../components/GlobalStyle';
-import ProgressBar from '../../components/ProgressBar';
-import SentencePaint from './SentencePaint';
-import LeftDoran from '../../components/LeftDoran';
-import { ToastContainer, toast } from 'react-toastify';
+import React, { useState } from "react";
+import styled from "styled-components";
+import Header from "../../components/Header";
+import GlobalStyle from "../../components/GlobalStyle";
+import ProgressBar from "../../components/ProgressBar";
+import SentencePaint from "./SentencePaint";
+import LeftDoran from "../../components/LeftDoran";
+import { ToastContainer, toast } from "react-toastify";
 
-import 'react-toastify/dist/ReactToastify.css';
-import { useNavigate } from 'react-router-dom';
-import Step1List from './Step1List';
+import "react-toastify/dist/ReactToastify.css";
+import { useNavigate } from "react-router-dom";
+import Step1List from "./Step1List";
 
 const MainBlock = styled.div`
-	.literally {
-		width: 862px;
-		min-height: 453px;
-	}
+    .literally {
+        width: 862px;
+        min-height: 453px;
+    }
 
-	.question,
-	.btn {
-		margin-left: 250px;
-	}
+    .question,
+    .btn {
+        margin-left: 250px;
+    }
 
-	.trash {
-		padding-left: 10px;
-		cursor: pointer;
-	}
+    .trash {
+        padding-left: 10px;
+        cursor: pointer;
+    }
 
-	.centercontent {
-		font-family: 'KOTRAHOPE';
-		display: flex;
-		flex-flow: row nowrap;
-		justify-content: space-around;
-		font-style: normal;
-		margin: 0 30px;
-		font-weight: 380;
-		font-size: 38px;
-		line-height: 48px;
-		text-align: center;
+    .centercontent {
+        font-family: "KOTRAHOPE";
+        display: flex;
+        flex-flow: row nowrap;
+        justify-content: space-around;
+        font-style: normal;
+        margin: 0 30px;
+        font-weight: 380;
+        font-size: 38px;
+        line-height: 48px;
+        text-align: center;
 
-		div {
-			display: inline-block;
-			vertical-align: middle;
-			margin: 40px 0;
-		}
+        div {
+            display: inline-block;
+            vertical-align: middle;
+            margin: 40px 0;
+        }
 
-		button {
-			display: inline-block;
-			vertical-align: middle;
-			margin: 10px 20px;
-		}
-	}
+        button {
+            display: inline-block;
+            vertical-align: middle;
+            margin: 10px 20px;
+        }
+    }
 
-	.paint {
-		text-align: center;
-		width: 862px;
-		margin: 0 auto;
-		margin-bottom: 50px;
-		.canvas {
-			.literally toolbar-at-top {
-				text-align: center;
-			}
-		}
-	}
+    .paint {
+        text-align: center;
+        width: 862px;
+        margin: 0 auto;
+        margin-bottom: 50px;
+        .canvas {
+            .literally toolbar-at-top {
+                text-align: center;
+            }
+        }
+    }
 
-	.buttonline {
-		text-align: center;
-		width: 212px;
-		margin: 20px auto;
-	}
+    .buttonline {
+        text-align: center;
+        width: 212px;
+        margin: 20px auto;
+    }
 
-	.sentencelist {
-		padding: 15px;
-		background: #f9de4b;
-		border: 3px solid #000000;
-		box-sizing: border-box;
-		border-radius: 32px;
-		font-family: 'KOTRAHOPE';
-		font-style: normal;
-		font-weight: 400;
-		font-size: 40px;
-		text-align: center;
-		display: flex;
-		align-items: center;
-		color: #000000;
-	}
+    .sentencelist {
+        padding: 15px;
+        background: #f9de4b;
+        border: 3px solid #000000;
+        box-sizing: border-box;
+        border-radius: 32px;
+        font-family: "KOTRAHOPE";
+        font-style: normal;
+        font-weight: 400;
+        font-size: 40px;
+        text-align: center;
+        display: flex;
+        align-items: center;
+        color: #000000;
+    }
 
-	.answer {
-		font-family: 'KOTRAHOPE';
-		font-style: normal;
-		font-weight: 400;
-		font-size: 25px;
-		line-height: 25px;
-	}
+    .answer {
+        font-family: "KOTRAHOPE";
+        font-style: normal;
+        font-weight: 400;
+        font-size: 25px;
+        line-height: 25px;
+    }
 
-	.answer {
-		display: inline-flex;
-		text-align: center;
-		align-items: center;
-	}
+    .answer {
+        display: inline-flex;
+        text-align: center;
+        align-items: center;
+    }
 
-	.onedit,
-	.offedit {
-		background: white;
-		box-sizing: border-box;
-		border-radius: 32px;
-		font-family: 'Cafe24Syongsyong';
-		font-style: normal;
-		font-size: 25px;
-		display: inline-flex;
-		width: 700px;
-		align-items: center;
-		color: #000000;
-		padding: 30px 15px;
-		margin-left: 10px;
-	}
+    .onedit,
+    .offedit {
+        background: white;
+        box-sizing: border-box;
+        border-radius: 32px;
+        font-family: "Cafe24Syongsyong";
+        font-style: normal;
+        font-size: 25px;
+        display: inline-flex;
+        width: 700px;
+        align-items: center;
+        color: #000000;
+        padding: 30px 15px;
+        margin-left: 10px;
+    }
 
-	.offedit {
-		height: 38px;
-	}
+    .offedit {
+        height: 38px;
+    }
 
-	.xbutton {
-		z-index: 5;
-		display: inline-block;
-		font-family: 'soojin';
-		font-size: 30px;
-		background: #e75244;
-		width: 50px;
-		height: 50px;
-		border-radius: 100%;
-		display: flex;
-		align-items: center;
-		justify-content: center;
-		text-align: center;
-		color: white;
-		margin-left: 5px;
-	}
+    .xbutton {
+        z-index: 5;
+        display: inline-block;
+        font-family: "soojin";
+        font-size: 30px;
+        background: #e75244;
+        width: 50px;
+        height: 50px;
+        border-radius: 100%;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        text-align: center;
+        color: white;
+        margin-left: 5px;
+    }
 
-	.description {
-		font-family: 'Cafe24Syongsyong';
-		font-style: normal;
-		font-size: 25px;
-		color: white;
-		line-height: 35px;
-	}
+    .description {
+        font-family: "Cafe24Syongsyong";
+        font-style: normal;
+        font-size: 25px;
+        color: white;
+        line-height: 35px;
+    }
 
-	.btn {
-		display: flex;
-		flex-flow: row nowrap;
-		justify-content: space-around;
-	}
+    .btn {
+        display: flex;
+        flex-flow: row nowrap;
+        justify-content: space-around;
+    }
 `;
 
 function Step1() {
-	const navigate = useNavigate('');
-	const StyledContainer = styled(ToastContainer)`
-		&&&.Toastify__toast-container {
-			bottom: 80px;
-			right: 20px;
-		}
-		.Toastify__toast {
-			font-size: 30px;
-		}
-		.Toastify__toast-body {
-			font-family: 'KOTRAHOPE';
-			font-style: normal;
-			font-size: 24px;
-			color: black;
-		}
-		.Toastify__progress-bar {
-		}
-	`;
+    const navigate = useNavigate("");
+    const StyledContainer = styled(ToastContainer)`
+        &&&.Toastify__toast-container {
+            bottom: 80px;
+            right: 20px;
+        }
+        .Toastify__toast {
+            font-size: 30px;
+        }
+        .Toastify__toast-body {
+            font-family: "KOTRAHOPE";
+            font-style: normal;
+            font-size: 24px;
+            color: black;
+        }
+        .Toastify__progress-bar {
+        }
+    `;
 
-	const nextNotify = () => {
-		toast.error('대답을 써야 다음 질문을 볼 수 있어요!', {
-			position: toast.POSITION.BOTTOM_RIGHT,
-			autoClose: 3000,
-		});
-	};
+    const nextNotify = () => {
+        toast.error("대답을 써야 다음 질문을 볼 수 있어요!", {
+            position: toast.POSITION.BOTTOM_RIGHT,
+            autoClose: 3000,
+        });
+    };
 
-	const levelNotify = () => {
-		toast.error('대답을 써야 다음 단계로 넘어갈 수 있어요!', {
-			position: toast.POSITION.BOTTOM_RIGHT,
-			autoClose: 3000,
-		});
-	};
+    const levelNotify = () => {
+        toast.error("대답을 써야 다음 단계로 넘어갈 수 있어요!", {
+            position: toast.POSITION.BOTTOM_RIGHT,
+            autoClose: 3000,
+        });
+    };
 
-	const prevNotify = () => {
-		toast.error('대답을 써야 이전 질문과 대답을 다시 볼 수 있어요!', {
-			position: toast.POSITION.BOTTOM_RIGHT,
-			autoClose: 3000,
-		});
-	};
+    const prevNotify = () => {
+        toast.error("대답을 써야 이전 질문과 대답을 다시 볼 수 있어요!", {
+            position: toast.POSITION.BOTTOM_RIGHT,
+            autoClose: 3000,
+        });
+    };
 
-	return (
-		<>
-			<GlobalStyle backColor="purple" />
-			<Header
-				isProgress
-				isLogout
-				isImgBtn
-				progress={
-					<ProgressBar
-						progressText={'4.일기쓰기'}
-						progressWidth={'50'}
-						progressColor={'#E75244'}
-					></ProgressBar>
-				}
-				backColor="purple"
-			/>
-			<MainBlock>
-				<LeftDoran />
-				<Step1List nextNotify={nextNotify} prevNotify={prevNotify} levelNotify={levelNotify} />
-				<div className="paint" style={{ paddingLeft: '250px' }}>
-					<SentencePaint />
-				</div>
-			</MainBlock>
-			<StyledContainer>
-				<ToastContainer />
-			</StyledContainer>
-		</>
-	);
+    return (
+        <>
+            <GlobalStyle backColor="purple" />
+            <Header
+                isProgress
+                isLogout
+                isImgBtn
+                progress={
+                    <ProgressBar
+                        progressText={"4.일기쓰기"}
+                        progressWidth={"56"}
+                        progressColor={"#E75244"}
+                    ></ProgressBar>
+                }
+                backColor="purple"
+            />
+            <MainBlock>
+                <LeftDoran />
+                <Step1List
+                    nextNotify={nextNotify}
+                    prevNotify={prevNotify}
+                    levelNotify={levelNotify}
+                />
+                <div className="paint" style={{ paddingLeft: "250px" }}>
+                    <SentencePaint />
+                </div>
+            </MainBlock>
+            <StyledContainer>
+                <ToastContainer />
+            </StyledContainer>
+        </>
+    );
 }
 
 export default Step1;
--- doranteacher-frontend/src/pages/title/Title.js
+++ doranteacher-frontend/src/pages/title/Title.js
@@ -1,239 +1,239 @@
-import React, { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
-import { toast, ToastContainer } from 'react-toastify';
-import styled from 'styled-components';
-import Button from '../../components/Button';
-import GlobalStyle from '../../components/GlobalStyle';
-import Header from '../../components/Header';
-import LeftDoran from '../../components/LeftDoran';
-import NextButton from '../../components/NextButton';
-import ProgressBar from '../../components/ProgressBar';
-import TitlePaint from './TitlePaint';
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { toast, ToastContainer } from "react-toastify";
+import styled from "styled-components";
+import Button from "../../components/Button";
+import GlobalStyle from "../../components/GlobalStyle";
+import Header from "../../components/Header";
+import LeftDoran from "../../components/LeftDoran";
+import NextButton from "../../components/NextButton";
+import ProgressBar from "../../components/ProgressBar";
+import TitlePaint from "./TitlePaint";
 
 const MainBlock = styled.div`
-	.literally {
-		width: 862px;
-		min-height: 243px;
-	}
+    .literally {
+        width: 862px;
+        min-height: 243px;
+    }
 
-	.question,
-	.btn {
-		margin-left: 250px;
-	}
+    .question,
+    .btn {
+        margin-left: 250px;
+    }
 
-	.trash {
-		padding-left: 10px;
-		cursor: pointer;
-	}
+    .trash {
+        padding-left: 10px;
+        cursor: pointer;
+    }
 
-	.centercontent {
-		font-family: 'KOTRAHOPE';
-		display: flex;
-		flex-flow: row nowrap;
-		justify-content: space-around;
-		font-style: normal;
-		margin: 0 30px;
-		font-weight: 380;
-		font-size: 38px;
-		line-height: 48px;
-		text-align: center;
+    .centercontent {
+        font-family: "KOTRAHOPE";
+        display: flex;
+        flex-flow: row nowrap;
+        justify-content: space-around;
+        font-style: normal;
+        margin: 0 30px;
+        font-weight: 380;
+        font-size: 38px;
+        line-height: 48px;
+        text-align: center;
 
-		div {
-			display: inline-block;
-			vertical-align: middle;
-			margin: 40px 0;
-		}
+        div {
+            display: inline-block;
+            vertical-align: middle;
+            margin: 40px 0;
+        }
 
-		button {
-			display: inline-block;
-			vertical-align: middle;
-			margin: 10px 20px;
-		}
-	}
+        button {
+            display: inline-block;
+            vertical-align: middle;
+            margin: 10px 20px;
+        }
+    }
 
-	.paint {
-		text-align: center;
-		width: 862px;
-		margin: 0 auto;
-		.canvas {
-			.literally toolbar-at-top {
-				text-align: center;
-			}
-		}
-	}
+    .paint {
+        text-align: center;
+        width: 862px;
+        margin: 0 auto;
+        .canvas {
+            .literally toolbar-at-top {
+                text-align: center;
+            }
+        }
+    }
 
-	.buttonline {
-		text-align: center;
-		width: 212px;
-		margin: 20px auto;
-	}
+    .buttonline {
+        text-align: center;
+        width: 212px;
+        margin: 20px auto;
+    }
 
-	.sentencelist {
-		padding: 15px;
-		background: #f9de4b;
-		border: 3px solid #000000;
-		box-sizing: border-box;
-		border-radius: 32px;
-		font-family: 'KOTRAHOPE';
-		font-style: normal;
-		font-weight: 400;
-		font-size: 40px;
-		text-align: center;
-		display: flex;
-		align-items: center;
-		color: #000000;
-	}
+    .sentencelist {
+        padding: 15px;
+        background: #f9de4b;
+        border: 3px solid #000000;
+        box-sizing: border-box;
+        border-radius: 32px;
+        font-family: "KOTRAHOPE";
+        font-style: normal;
+        font-weight: 400;
+        font-size: 40px;
+        text-align: center;
+        display: flex;
+        align-items: center;
+        color: #000000;
+    }
 
-	.answer {
-		font-family: 'KOTRAHOPE';
-		font-style: normal;
-		font-weight: 400;
-		font-size: 25px;
-		line-height: 25px;
-	}
+    .answer {
+        font-family: "KOTRAHOPE";
+        font-style: normal;
+        font-weight: 400;
+        font-size: 25px;
+        line-height: 25px;
+    }
 
-	.answer {
-		display: inline-flex;
-		text-align: center;
-		align-items: center;
-	}
+    .answer {
+        display: inline-flex;
+        text-align: center;
+        align-items: center;
+    }
 
-	.onedit,
-	.offedit {
-		background: white;
-		box-sizing: border-box;
-		border-radius: 32px;
-		font-family: 'Cafe24Syongsyong';
-		font-style: normal;
-		font-size: 25px;
-		display: inline-flex;
-		width: 700px;
-		align-items: center;
-		color: #000000;
-		padding: 30px 15px;
-		margin-left: 10px;
-	}
+    .onedit,
+    .offedit {
+        background: white;
+        box-sizing: border-box;
+        border-radius: 32px;
+        font-family: "Cafe24Syongsyong";
+        font-style: normal;
+        font-size: 25px;
+        display: inline-flex;
+        width: 700px;
+        align-items: center;
+        color: #000000;
+        padding: 30px 15px;
+        margin-left: 10px;
+    }
 
-	.offedit {
-		height: 38px;
-	}
+    .offedit {
+        height: 38px;
+    }
 
-	.xbutton {
-		z-index: 5;
-		display: inline-block;
-		font-family: 'soojin';
-		font-size: 30px;
-		background: #e75244;
-		width: 50px;
-		height: 50px;
-		border-radius: 100%;
-		display: flex;
-		align-items: center;
-		justify-content: center;
-		text-align: center;
-		color: white;
-		margin-left: 5px;
-	}
+    .xbutton {
+        z-index: 5;
+        display: inline-block;
+        font-family: "soojin";
+        font-size: 30px;
+        background: #e75244;
+        width: 50px;
+        height: 50px;
+        border-radius: 100%;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        text-align: center;
+        color: white;
+        margin-left: 5px;
+    }
 
-	.description {
-		font-family: 'Cafe24Syongsyong';
-		font-style: normal;
-		font-size: 25px;
-		color: white;
-		line-height: 35px;
-	}
+    .description {
+        font-family: "Cafe24Syongsyong";
+        font-style: normal;
+        font-size: 25px;
+        color: white;
+        line-height: 35px;
+    }
 
-	.btn {
-		display: flex;
-		flex-flow: row nowrap;
-		justify-content: space-around;
-	}
+    .btn {
+        display: flex;
+        flex-flow: row nowrap;
+        justify-content: space-around;
+    }
 
-	.container {
-		margin-top: 30px;
-	}
+    .container {
+        margin-top: 30px;
+    }
 `;
 
 function Title() {
-	const navigate = useNavigate('');
-	const StyledContainer = styled(ToastContainer)`
-		&&&.Toastify__toast-container {
-			bottom: 80px;
-			right: 20px;
-		}
-		.Toastify__toast {
-			font-size: 30px;
-		}
-		.Toastify__toast-body {
-			font-family: 'KOTRAHOPE';
-			font-style: normal;
-			font-size: 24px;
-			color: black;
-		}
-		.Toastify__progress-bar {
-		}
-	`;
-	const NextButtonStyle = styled.div`
-		text-align: center;
-		padding: 20px 0;
-	`;
+    const navigate = useNavigate("");
+    const StyledContainer = styled(ToastContainer)`
+        &&&.Toastify__toast-container {
+            bottom: 80px;
+            right: 20px;
+        }
+        .Toastify__toast {
+            font-size: 30px;
+        }
+        .Toastify__toast-body {
+            font-family: "KOTRAHOPE";
+            font-style: normal;
+            font-size: 24px;
+            color: black;
+        }
+        .Toastify__progress-bar {
+        }
+    `;
+    const NextButtonStyle = styled.div`
+        text-align: center;
+        padding: 20px 0;
+    `;
 
-	const notify = () => {
-		console.log(document.querySelector('p'));
-		toast.error('제목을 작성해야 다음 단계로 넘어갈 수 있어요!', {
-			position: toast.POSITION.BOTTOM_RIGHT,
-			autoClose: 3000,
-		});
-	};
+    const notify = () => {
+        console.log(document.querySelector("p"));
+        toast.error("제목을 작성해야 다음 단계로 넘어갈 수 있어요!", {
+            position: toast.POSITION.BOTTOM_RIGHT,
+            autoClose: 3000,
+        });
+    };
 
-	function checking() {
-		const check = document.querySelector('p');
-		if (check.innerText === '') {
-			notify();
-		} else {
-			navigate('/writing/save');
-		}
-	}
+    function checking() {
+        const check = document.querySelector("p");
+        if (check.innerText === "") {
+            notify();
+        } else {
+            navigate("/writing/save");
+        }
+    }
 
-	return (
-		<div>
-			<GlobalStyle backColor="green" />
-			<Header
-				isProgress
-				isLogout
-				isImgBtn
-				progress={
-					<ProgressBar
-						progressText={'6.제목쓰기'}
-						progressWidth={'70'}
-						progressColor={'#E75244'}
-					></ProgressBar>
-				}
-				backColor="green"
-			/>
-			<MainBlock>
-				<LeftDoran />
-				<div className="question">
-					<div className="centercontent">
-						<div className="questioncontent">
-							오늘 일기와 가장 어울리는
-							<br />
-							제목을 써 볼까요?
-						</div>
-					</div>
-				</div>
-				<div className="paint" style={{ paddingLeft: '250px' }}>
-					<TitlePaint />
-					<div className="nextBtn">
-						<NextButton onClick={checking} />
-					</div>
-				</div>
-			</MainBlock>
-			<StyledContainer>
-				<ToastContainer />
-			</StyledContainer>
-		</div>
-	);
+    return (
+        <div>
+            <GlobalStyle backColor="green" />
+            <Header
+                isProgress
+                isLogout
+                isImgBtn
+                progress={
+                    <ProgressBar
+                        progressText={"6.제목쓰기"}
+                        progressWidth={"84"}
+                        progressColor={"#E75244"}
+                    ></ProgressBar>
+                }
+                backColor="green"
+            />
+            <MainBlock>
+                <LeftDoran />
+                <div className="question">
+                    <div className="centercontent">
+                        <div className="questioncontent">
+                            오늘 일기와 가장 어울리는
+                            <br />
+                            제목을 써 볼까요?
+                        </div>
+                    </div>
+                </div>
+                <div className="paint" style={{ paddingLeft: "250px" }}>
+                    <TitlePaint />
+                    <div className="nextBtn">
+                        <NextButton onClick={checking} />
+                    </div>
+                </div>
+            </MainBlock>
+            <StyledContainer>
+                <ToastContainer />
+            </StyledContainer>
+        </div>
+    );
 }
 
 export default Title;
