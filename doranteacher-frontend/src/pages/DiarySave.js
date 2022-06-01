e352b6bd384c82e0a5c827ac780b4954c9812f82
Koeunseooooo <rhdmstj17@cau.ac.kr>
2022-06-01 15:23:53 +0900

    Feat: 사진 업로드 시 상태 변경하도록 추가

--- doranteacher-frontend/src/pages/DiarySave.js
+++ doranteacher-frontend/src/pages/DiarySave.js
@@ -1,248 +1,257 @@
-import React, { useState } from 'react';
-import styled, { css, createGlobalStyle } from 'styled-components';
-import Header from '../components/Header';
-import Button from '../components/Button';
-import GlobalStyle from '../components/GlobalStyle';
-import { useNavigate, Link } from 'react-router-dom';
-import LeftDoran from '../components/LeftDoran';
-import ProgressBar from '../components/ProgressBar';
+import React, { useState } from "react";
+import styled, { css, createGlobalStyle } from "styled-components";
+import Header from "../components/Header";
+import Button from "../components/Button";
+import GlobalStyle from "../components/GlobalStyle";
+import { useNavigate, Link } from "react-router-dom";
+import LeftDoran from "../components/LeftDoran";
+import ProgressBar from "../components/ProgressBar";
 
 const MainBlock = styled.div`
-	display: flex;
-	flex-direction: column;
-	align-items: center;
-	.content {
-		margin-top: 30px;
-		margin-bottom: 20px;
-		font-family: 'KOTRAHOPE';
-		font-style: normal;
-		font-weight: 380;
-		font-size: 35px;
-		line-height: 48px;
-		text-align: center;
-	}
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    .content {
+        margin-top: 30px;
+        margin-bottom: 20px;
+        font-family: "KOTRAHOPE";
+        font-style: normal;
+        font-weight: 380;
+        font-size: 35px;
+        line-height: 48px;
+        text-align: center;
+    }
 
-	.question {
-		margin-top: 25px;
-		margin-bottom: 20px;
-		font-family: 'KOTRAHOPE';
-		font-style: normal;
-		font-weight: 380;
-		font-size: 25px;
-		line-height: 48px;
-		text-align: center;
-	}
+    .question {
+        margin-top: 25px;
+        margin-bottom: 20px;
+        font-family: "KOTRAHOPE";
+        font-style: normal;
+        font-weight: 380;
+        font-size: 25px;
+        line-height: 48px;
+        text-align: center;
+    }
 
-	.yellowbox {
-		display: flex;
-		flex-direction: column;
-		align-items: center;
-		padding-top: 20px;
+    .yellowbox {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        padding-top: 20px;
 
-		width: 600px;
-		height: 480px;
-		background: #f9de4b;
-		outline: 0;
-		border: 0;
-		letter-spacing: 1px;
-		position: relative;
+        width: 600px;
+        height: 480px;
+        background: #f9de4b;
+        outline: 0;
+        border: 0;
+        letter-spacing: 1px;
+        position: relative;
 
-		border-radius: 25px;
-		border: 2px solid black;
-		transition: transform 0.2s cubic-bezier(0, 0, 0.7, 1);
+        border-radius: 25px;
+        border: 2px solid black;
+        transition: transform 0.2s cubic-bezier(0, 0, 0.7, 1);
 
-		&:before {
-			z-index: -1;
-			position: absolute;
-			top: 0;
-			right: 0;
-			bottom: 0;
-			left: 0;
-			content: '';
-			width: 100%;
-			height: 100%;
-			position: absolute;
-			background: white;
-			transform: translate3d(0.2em, 0.15em, 1em);
-			border-radius: 25px;
-			border: 2px solid black;
-			transition: transform 0.2s cubic-bezier(0, 0, 0.7, 1);
-			&:active {
-				z-index: -1;
-			}
-		}
-	}
+        &:before {
+            z-index: -1;
+            position: absolute;
+            top: 0;
+            right: 0;
+            bottom: 0;
+            left: 0;
+            content: "";
+            width: 100%;
+            height: 100%;
+            position: absolute;
+            background: white;
+            transform: translate3d(0.2em, 0.15em, 1em);
+            border-radius: 25px;
+            border: 2px solid black;
+            transition: transform 0.2s cubic-bezier(0, 0, 0.7, 1);
+            &:active {
+                z-index: -1;
+            }
+        }
+    }
 
-	.buttons {
-		display: flex;
-	}
+    .buttons {
+        display: flex;
+    }
 
-	.buttonStyle {
-		margin-left: 10px;
-		margin-right: 10px;
-	}
+    .buttonStyle {
+        margin-left: 10px;
+        margin-right: 10px;
+    }
 
-	.saveButton {
-		margin-top: 15px;
-	}
+    .saveButton {
+        margin-top: 15px;
+    }
 
-	.key {
-		color: #e75244;
-	}
+    .key {
+        color: #e75244;
+    }
 
-	.nextButton {
-		align-self: flex-end;
-		margin-top: 20px;
-		margin-right: 70px;
-	}
+    .nextButton {
+        align-self: flex-end;
+        margin-top: 20px;
+        margin-right: 70px;
+    }
 
-	.on {
-		background: #e75244;
-		transition: all 0.1s cubic-bezier(0, 0, 0.7, 1);
-		top: 4px;
-		left: 3.5px;
-		&:before {
-			top: -4px;
-			left: -4.7px;
-		}
-	}
+    .on {
+        background: #e75244;
+        transition: all 0.1s cubic-bezier(0, 0, 0.7, 1);
+        top: 4px;
+        left: 3.5px;
+        &:before {
+            top: -4px;
+            left: -4.7px;
+        }
+    }
 
-	// input[type="file"] {
-	//     position: absolute;
-	//     width: 0;
-	//     height: 0;
-	//     padding: 0;
-	//     margin: -1px;
-	//     overflow: hidden;
-	//     clip: rect(0, 0, 0, 0);
-	//     border: 0;
-	// }
+    // input[type="file"] {
+    //     position: absolute;
+    //     width: 0;
+    //     height: 0;
+    //     padding: 0;
+    //     margin: -1px;
+    //     overflow: hidden;
+    //     clip: rect(0, 0, 0, 0);
+    //     border: 0;
+    // }
 
-	.imageUpload {
-		padding-left: 280px;
-		margin-top: 20px;
-		margin-bottom: -20px;
-	}
+    .imageUpload {
+        padding-left: 280px;
+        margin-top: 20px;
+        margin-bottom: -20px;
+    }
 `;
 
 function DiarySave() {
-	// console.log(getStringDate(new Date()));
-	let [painting, setPainting] = useState(true);
-	let [correct, setCorrect] = useState(true);
-	let [comment, setComment] = useState(true);
+    // console.log(getStringDate(new Date()));
+    let [painting, setPainting] = useState(true);
+    let [correct, setCorrect] = useState(true);
+    let [comment, setComment] = useState(true);
+    const [file, setFile] = useState(null);
 
-	// const [file, setFile] = useState("");
-	console.log(painting);
-	console.log(correct);
-	console.log(comment);
+    console.log(painting);
+    console.log(correct);
+    console.log(comment);
+    console.log(file);
 
-	const navigate = useNavigate('');
-	return (
-		<>
-			<GlobalStyle backColor="yellow" />
-			<LeftDoran />
-			<Header
-				isProgress
-				isLogout
-				isImgBtn
-				progress={
-					<ProgressBar
-						progressText={'7.일기저장'}
-						progressWidth={'87.5'}
-						progressColor={'#E75244'}
-						backColor="red"
-					></ProgressBar>
-				}
-			/>
-			<MainBlock>
-				<div className="content">
-					거의 다 끝났어요!
-					<br />
-					아래의 질문에 대답해볼까요?
-				</div>
-				<div className="yellowbox">
-					<div className="question">
-						Q1. 일기와 어울리는 <span className="key">그림</span>을 추천해줄까요?
-					</div>
-					<div className="buttons">
-						<div className="buttonStyle">
-							<Button
-								buttonText="네"
-								inputColor="purple"
-								extraClassName={painting === true ? `on` : ''}
-								onClick={() => setPainting(!painting)}
-							></Button>
-						</div>
-						<div className="buttonStyle">
-							<Button
-								buttonText="사진업로드"
-								inputColor="purple"
-								extraClassName={painting === false ? 'on' : ''}
-								onClick={() => setPainting(!painting)}
-							/>
-						</div>
-					</div>
+    const navigate = useNavigate("");
+    return (
+        <>
+            <GlobalStyle backColor="yellow" />
+            <LeftDoran />
+            <Header
+                isProgress
+                isLogout
+                isImgBtn
+                progress={
+                    <ProgressBar
+                        progressText={"7.일기저장"}
+                        progressWidth={"87.5"}
+                        progressColor={"#E75244"}
+                        backColor="red"
+                    ></ProgressBar>
+                }
+            />
+            <MainBlock>
+                <div className="content">
+                    거의 다 끝났어요!
+                    <br />
+                    아래의 질문에 대답해볼까요?
+                </div>
+                <div className="yellowbox">
+                    <div className="question">
+                        Q1. 일기와 어울리는 <span className="key">그림</span>을
+                        추천해줄까요?
+                    </div>
+                    <div className="buttons">
+                        <div className="buttonStyle">
+                            <Button
+                                buttonText="네"
+                                inputColor="purple"
+                                extraClassName={painting === true ? `on` : ""}
+                                onClick={() => setPainting(!painting)}
+                            ></Button>
+                        </div>
+                        <div className="buttonStyle">
+                            <Button
+                                buttonText="사진업로드"
+                                inputColor="purple"
+                                extraClassName={painting === false ? "on" : ""}
+                                onClick={() => setPainting(!painting)}
+                            />
+                        </div>
+                    </div>
 
-					<div className="imageUpload">
-						{!painting ? (
-							<input
-								type="file"
-								id="ex_file"
-								accept="image/jpg, image/png, image/jpeg"
-								onChange={(e) => console.log(e.target.files[0])}
-							/>
-						) : (
-							''
-						)}
-					</div>
-					<div className="question">
-						Q2. 도란쌤의 <span className="key">맞춤법 교정</span>을 원하나요?
-					</div>
-					<div className="buttons">
-						<div className="buttonStyle">
-							<Button
-								buttonText="네"
-								inputColor="purple"
-								extraClassName={correct === true ? `on` : ''}
-								onClick={() => setCorrect(!correct)}
-							></Button>
-						</div>
-						<div className="buttonStyle">
-							<Button
-								buttonText="아니요"
-								inputColor="purple"
-								extraClassName={correct === false ? 'on' : ''}
-								onClick={() => setCorrect(!correct)}
-							></Button>
-						</div>
-					</div>
-					<div className="question">
-						Q3. 도란쌤의 <span className="key">답변</span>를 받고 싶나요?
-					</div>
-					<div className="buttons">
-						<div className="buttonStyle">
-							<Button
-								buttonText="네"
-								inputColor="purple"
-								extraClassName={comment === true ? `on` : ''}
-								onClick={() => setComment(!comment)}
-							></Button>
-						</div>
-						<div className="buttonStyle">
-							<Button
-								buttonText="아니요"
-								inputColor="purple"
-								extraClassName={comment === false ? 'on' : ''}
-								onClick={() => setComment(!comment)}
-							></Button>
-						</div>
-					</div>
-				</div>
-				<div className="nextButton">
-					<Button buttonText="끝!" type="submit" outputColor="purple" onClick={() => navigate('/')}></Button>
-				</div>
-			</MainBlock>
-		</>
-	);
+                    <div className="imageUpload">
+                        {!painting ? (
+                            <input
+                                type="file"
+                                id="ex_file"
+                                accept="image/jpg, image/png, image/jpeg"
+                                onChange={(e) => setFile(e.target.files[0])}
+                            />
+                        ) : (
+                            ""
+                        )}
+                    </div>
+                    <div className="question">
+                        Q2. 도란쌤의 <span className="key">맞춤법 교정</span>을
+                        원하나요?
+                    </div>
+                    <div className="buttons">
+                        <div className="buttonStyle">
+                            <Button
+                                buttonText="네"
+                                inputColor="purple"
+                                extraClassName={correct === true ? `on` : ""}
+                                onClick={() => setCorrect(!correct)}
+                            ></Button>
+                        </div>
+                        <div className="buttonStyle">
+                            <Button
+                                buttonText="아니요"
+                                inputColor="purple"
+                                extraClassName={correct === false ? "on" : ""}
+                                onClick={() => setCorrect(!correct)}
+                            ></Button>
+                        </div>
+                    </div>
+                    <div className="question">
+                        Q3. 도란쌤의 <span className="key">답변</span>를 받고
+                        싶나요?
+                    </div>
+                    <div className="buttons">
+                        <div className="buttonStyle">
+                            <Button
+                                buttonText="네"
+                                inputColor="purple"
+                                extraClassName={comment === true ? `on` : ""}
+                                onClick={() => setComment(!comment)}
+                            ></Button>
+                        </div>
+                        <div className="buttonStyle">
+                            <Button
+                                buttonText="아니요"
+                                inputColor="purple"
+                                extraClassName={comment === false ? "on" : ""}
+                                onClick={() => setComment(!comment)}
+                            ></Button>
+                        </div>
+                    </div>
+                </div>
+                <div className="nextButton">
+                    <Button
+                        buttonText="끝!"
+                        type="submit"
+                        outputColor="purple"
+                        onClick={() => navigate("/")}
+                    ></Button>
+                </div>
+            </MainBlock>
+        </>
+    );
 }
 export default DiarySave;
