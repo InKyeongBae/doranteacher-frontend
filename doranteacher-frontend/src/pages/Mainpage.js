ca42db971e9325cdf1c7685e864a5d5913f6c8e4
InKyeongBae <inkkyung@gmail.com>
2022-06-01 12:55:58 +0900

    Feat : 로그아웃 시 로컬스토리지 초기화 구현

--- doranteacher-frontend/src/pages/Mainpage.js
+++ doranteacher-frontend/src/pages/Mainpage.js
@@ -1,233 +1,231 @@
-import React, { useState, useEffect } from "react";
-import styled, { css, createGlobalStyle } from "styled-components";
-import Header from "../components/Header";
-import ShakingHands from "../components/ShakingHands";
-import GlobalStyle from "../components/GlobalStyle";
-import { useNavigate, Link } from "react-router-dom";
-import { useCookies } from "react-cookie";
+import React, { useState, useEffect } from 'react';
+import styled, { css, createGlobalStyle } from 'styled-components';
+import Header from '../components/Header';
+import ShakingHands from '../components/ShakingHands';
+import GlobalStyle from '../components/GlobalStyle';
+import { useNavigate, Link } from 'react-router-dom';
+import { useCookies } from 'react-cookie';
+import axios from 'axios';
 
 const MainBlock = styled.div`
-    background: #f9de4b;
+	background: #f9de4b;
 `;
 
 const StyledLink = styled(Link)`
-    text-decoration: none;
-    color: black;
-    &:focus,
-    &:hover,
-    &:visited,
-    &:link,
-    &:active {
-        text-decoration: none;
-        color: black;
-    }
+	text-decoration: none;
+	color: black;
+	&:focus,
+	&:hover,
+	&:visited,
+	&:link,
+	&:active {
+		text-decoration: none;
+		color: black;
+	}
 `;
 
 const CenterLogo = styled.div`
-    text-align: center;
+	text-align: center;
 
-    img {
-        width: 400px;
-        padding: 30px 0;
-    }
+	img {
+		width: 400px;
+		padding: 30px 0;
+	}
 
-    .centercontent {
-        font-family: "KOTRAHOPE";
-        font-style: normal;
-        font-weight: 400;
-        font-size: 30px;
-        line-height: 30px;
-    }
+	.centercontent {
+		font-family: 'KOTRAHOPE';
+		font-style: normal;
+		font-weight: 400;
+		font-size: 30px;
+		line-height: 30px;
+	}
 `;
 
 const BottomBlock = styled.div`
-    position: fixed;
-    bottom: 0;
+	position: fixed;
+	bottom: 0;
 
-    .block {
-        width: 100%;
-        display: flex;
+	.block {
+		width: 100%;
+		display: flex;
 
-        @keyframes block-ani1 {
-            11% {
-                transform: scale(1);
-            }
-            22% {
-                transform: scale(1.1);
-            }
-            33% {
-                transform: scale(1);
-            }
-        }
+		@keyframes block-ani1 {
+			11% {
+				transform: scale(1);
+			}
+			22% {
+				transform: scale(1.1);
+			}
+			33% {
+				transform: scale(1);
+			}
+		}
 
-        @keyframes block-ani2 {
-            0% {
-                transform: scale(1.05);
-            }
-            44% {
-                transform: scale(1.05);
-            }
-            55% {
-                transform: scale(1.15);
-            }
-            66% {
-                transform: scale(1.05);
-            }
-            100% {
-                transform: scale(1.05);
-            }
-        }
+		@keyframes block-ani2 {
+			0% {
+				transform: scale(1.05);
+			}
+			44% {
+				transform: scale(1.05);
+			}
+			55% {
+				transform: scale(1.15);
+			}
+			66% {
+				transform: scale(1.05);
+			}
+			100% {
+				transform: scale(1.05);
+			}
+		}
 
-        @keyframes block-ani3 {
-            0% {
-                transform: scale(1.05);
-            }
-            77% {
-                transform: scale(1.05);
-            }
-            88% {
-                transform: scale(1.15);
-            }
-            99% {
-                transform: scale(1.05);
-            }
-            100% {
-                transform: scale(1.05);
-            }
-        }
+		@keyframes block-ani3 {
+			0% {
+				transform: scale(1.05);
+			}
+			77% {
+				transform: scale(1.05);
+			}
+			88% {
+				transform: scale(1.15);
+			}
+			99% {
+				transform: scale(1.05);
+			}
+			100% {
+				transform: scale(1.05);
+			}
+		}
 
-        .imgblock {
-            width: 100%;
-            height: 100%;
-            object-fit: contain;
-            object-position: bottom right;
-        }
+		.imgblock {
+			width: 100%;
+			height: 100%;
+			object-fit: contain;
+			object-position: bottom right;
+		}
 
-        .imgblock2 {
-            width: 100%;
-            height: 100%;
-            object-fit: contain;
-            object-position: bottom right;
-            cursor: pointer;
-            filter: drop-shadow(5px 5px 4px rgba(0, 0, 0, 0.3));
-        }
+		.imgblock2 {
+			width: 100%;
+			height: 100%;
+			object-fit: contain;
+			object-position: bottom right;
+			cursor: pointer;
+			filter: drop-shadow(5px 5px 4px rgba(0, 0, 0, 0.3));
+		}
 
-        #blockani1 {
-            animation: block-ani1 2.5s infinite;
-        }
+		#blockani1 {
+			animation: block-ani1 2.5s infinite;
+		}
 
-        #blockani2 {
-            animation: block-ani2 2.5s infinite;
-        }
+		#blockani2 {
+			animation: block-ani2 2.5s infinite;
+		}
 
-        #blockani3 {
-            animation: block-ani3 2.5s infinite;
-        }
-    }
+		#blockani3 {
+			animation: block-ani3 2.5s infinite;
+		}
+	}
 
-    .block div:first-child {
-        flex: 14;
-    }
+	.block div:first-child {
+		flex: 14;
+	}
 
-    .block div:nth-child(2) {
-        flex: 5;
-    }
+	.block div:nth-child(2) {
+		flex: 5;
+	}
 
-    .block div:nth-child(3) {
-        flex: 4;
-    }
+	.block div:nth-child(3) {
+		flex: 4;
+	}
 
-    .block div:nth-child(4) {
-        flex: 5;
-    }
+	.block div:nth-child(4) {
+		flex: 5;
+	}
 `;
 
 function Mainpage() {
-    const navigate = useNavigate();
-    const [cookies] = useCookies(["accessToken"]);
-    // console.log(cookies.acessToken !== undefined);
-    const [isLogin, setIsLogin] = useState(false);
+	const navigate = useNavigate();
+	const [cookies] = useCookies(['accessToken']);
+	// console.log(cookies.acessToken !== undefined);
+	const [isLogin, setIsLogin] = useState(false);
 
-    useEffect(() => {
-        if (cookies.accessToken !== undefined) {
-            // sessionStorage 에 user_id 라는 key 값으로 저장된 값이 없다면
-            console.log("isLogin ??? :: ", isLogin);
-            // 로그인 상태 변경
-            setIsLogin(true);
-        } else {
-            // sessionStorage 에 user_id 라는 key 값으로 저장된 값이 있다면
-            console.log("isLogin ?? :: ", isLogin);
-            setIsLogin(false);
-        }
-    });
-    return (
-        <>
-            <GlobalStyle backColor="yellow" />
-            {isLogin ? (
-                <Header isIcon isLogout isSetting backColor="yellow" />
-            ) : (
-                <Header isIcon isLogin isSignup isSetting backColor="yellow" />
-            )}
+	useEffect(() => {
+		if (cookies.accessToken !== undefined) {
+			// sessionStorage 에 user_id 라는 key 값으로 저장된 값이 없다면
+			console.log('isLogin ??? :: ', isLogin);
+			// 로그인 상태 변경
+			setIsLogin(true);
+		} else {
+			// sessionStorage 에 user_id 라는 key 값으로 저장된 값이 있다면
+			console.log('isLogin ?? :: ', isLogin);
+			setIsLogin(false);
+		}
+	});
 
-            <MainBlock>
-                <CenterLogo>
-                    <div className="centerlogo">
-                        <img
-                            className="doranLogo"
-                            src="/img/doranlogo.png"
-                            onClick={() => navigate("/")}
-                        />
-                    </div>
-                    <div className="centercontent">
-                        AI 도란쌤과 함께
-                        <br />
-                        일기 마스터하기
-                    </div>
-                </CenterLogo>
-                <BottomBlock>
-                    <div className="block">
-                        <div>
-                            <img className="imgblock" src="/img/block1.png" />
-                        </div>
-                        <div>
-                            <img
-                                className="imgblock2"
-                                id="blockani1"
-                                src="/img/block2.png"
-                            />
-                        </div>
+	function writeStart() {
+		axios
+			.get('http://3.39.158.98:8080/user/me')
+			.then((res) => localStorage.setItem('step', res.data.results[0].writingStep))
+			.then(() => navigate('/writing/start'));
+	}
+	return (
+		<>
+			<GlobalStyle backColor="yellow" />
+			{isLogin ? (
+				<Header isIcon isLogout isSetting backColor="yellow" />
+			) : (
+				<Header isIcon isLogin isSignup isSetting backColor="yellow" />
+			)}
 
-                        <div>
-                            <img className="imgblock" src="/img/block3.png" />
-                        </div>
-                        <div>
-                            <div>
-                                <img
-                                    className="imgblock2"
-                                    id="blockani2"
-                                    src="/img/block4-1.png"
-                                    onClick={() => navigate("/diary-list")}
-                                />
-                            </div>
-                            <div>
-                                <div>
-                                    <img
-                                        className="imgblock2"
-                                        id="blockani3"
-                                        src="/img/block4-2.png"
-                                        onClick={() =>
-                                            navigate("/writing/start")
-                                        }
-                                    />
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </BottomBlock>
-                <ShakingHands />
-            </MainBlock>
-        </>
-    );
+			<MainBlock>
+				<CenterLogo>
+					<div className="centerlogo">
+						<img className="doranLogo" src="/img/doranlogo.png" onClick={() => navigate('/')} />
+					</div>
+					<div className="centercontent">
+						AI 도란쌤과 함께
+						<br />
+						일기 마스터하기
+					</div>
+				</CenterLogo>
+				<BottomBlock>
+					<div className="block">
+						<div>
+							<img className="imgblock" src="/img/block1.png" />
+						</div>
+						<div>
+							<img className="imgblock2" id="blockani1" src="/img/block2.png" />
+						</div>
+
+						<div>
+							<img className="imgblock" src="/img/block3.png" />
+						</div>
+						<div>
+							<div>
+								<img
+									className="imgblock2"
+									id="blockani2"
+									src="/img/block4-1.png"
+									onClick={() => navigate('/diary-list')}
+								/>
+							</div>
+							<div>
+								<div>
+									<img
+										className="imgblock2"
+										id="blockani3"
+										src="/img/block4-2.png"
+										onClick={writeStart}
+									/>
+								</div>
+							</div>
+						</div>
+					</div>
+				</BottomBlock>
+				<ShakingHands />
+			</MainBlock>
+		</>
+	);
 }
 
 export default Mainpage;
