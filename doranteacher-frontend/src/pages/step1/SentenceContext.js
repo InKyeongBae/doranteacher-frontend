35131977b91ad95216badfe24e3693209a59addf
InKyeongBae <inkkyung@gmail.com>
2022-06-03 01:50:46 +0900

    Feat : Step1 질문 수정

--- doranteacher-frontend/src/pages/step1/SentenceContext.js
+++ doranteacher-frontend/src/pages/step1/SentenceContext.js
@@ -3,31 +3,31 @@ import React, { useReducer, createContext, useContext, useRef } from 'react';
 const initialSentences = [
 	{
 		id: 1,
-		question: 'Q1. 오늘 칭찬하고 싶은 사람은 누군가요?',
+		question: 'Q1. 오늘 본 영상의 제목이 무엇인가요?',
 		answer: '',
 		active: true,
 	},
 	{
 		id: 2,
-		question: 'Q2. 왜 그 사람을 칭찬하고 싶나요?',
+		question: 'Q2. 영상을 보게 된 계기가 있나요?',
 		answer: '',
 		active: false,
 	},
 	{
 		id: 3,
-		question: 'Q3. 그 사람의 행동을 봤을 때 어떤 기분이 들었나요?',
+		question: 'Q3. 가장 기억에 남는 장면이 무엇인가요? 왜 기억이 남았나요?',
 		answer: '',
 		active: false,
 	},
 	{
 		id: 4,
-		question: 'Q4. 그 사람에게 칭찬하는 말을 한 번 남겨볼까요?',
+		question: 'Q4. 영상을 보고 어떤 생각이 들었나요?',
 		answer: '',
 		active: false,
 	},
 	{
 		id: 5,
-		question: 'Q5. 방금 칭찬을 하면서 어떤 기분이 들었나요?',
+		question: 'Q5. 오늘 본 영상을 주변 사람들에게 소개한다면 어떻게 말할 수 있을까요?',
 		answer: '',
 		active: false,
 	},
@@ -44,9 +44,13 @@ function sentenceReducer(state, action) {
 		// 		),
 		// 	};
 		case 'CHANGE_ANSWER':
-			return state.map((sentence) => sentence.id === action.sentence.id ? { ...sentence, answer: action.sentence.answer} : sentence)
+			return state.map((sentence) =>
+				sentence.id === action.sentence.id ? { ...sentence, answer: action.sentence.answer } : sentence,
+			);
 		case 'REMOVE_ANSWER':
-			return state.map((sentence) => sentence.id === action.sentence.id ? { ...sentence, answer: ""} : sentence)
+			return state.map((sentence) =>
+				sentence.id === action.sentence.id ? { ...sentence, answer: '' } : sentence,
+			);
 		case 'CHANGE_ACTIVE':
 			return state.map((sentence) =>
 				sentence.id === action.originId
@@ -54,7 +58,7 @@ function sentenceReducer(state, action) {
 					: sentence.id === action.nextId
 					? { ...sentence, active: true }
 					: sentence,
-			)
+			);
 		default:
 			throw new Error(`Unhandled action type: ${action.type}`);
 	}
